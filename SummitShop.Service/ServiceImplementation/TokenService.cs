45608b04bd1610c75b3c1c6272d5f356d60cf97c
Mustafa Elkholy <mustafaelkholy@axlr8.tech>
2024-09-15 22:45:15 +0300

    Build The JWT Token

--- SummitShop.Service/ServiceImplementation/TokenService.cs
+++ SummitShop.Service/ServiceImplementation/TokenService.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SummitShop.Core.Entities.Identity;
 using SummitShop.Core.Services;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
@@ -12,6 +15,12 @@ namespace SummitShop.Service.ServiceImplementation
 {
     public class TokenService : ITokenService
     {
+        private readonly IConfiguration configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public async Task<string> CreateTokenAsync(ApplicationUser user, UserManager<ApplicationUser> userManager)
         {
             var authClaims = new List<Claim>()
@@ -21,11 +30,22 @@ namespace SummitShop.Service.ServiceImplementation
             };
 
             var userRoles = await userManager.GetRolesAsync(user);
+
             foreach (var role in userRoles)
                 authClaims.Add(new Claim(ClaimTypes.Role, role));
-               
 
+            var authKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]));
 
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:ValidIssuer"],
+                audience: configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddDays(double.Parse(configuration["JWT:DurationInDays"])),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authKey, SecurityAlgorithms.HmacSha256Signature)
+                );
+
+            var tokenGenerated = new JwtSecurityTokenHandler().WriteToken(token);
+            return tokenGenerated;
 
         }
     }
